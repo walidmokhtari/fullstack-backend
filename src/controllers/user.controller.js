f4457f7b1e47519dc2d327db3cb3adddb04dae4f
Vincent Zerbib <contact@zelabstudio.com>
2021-12-16 15:40:15 +0100

    save work

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,13 @@
 const User = require('../models/user.model');
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const configs = require('../configs');
 exports.register = (req, res) => {
     console.log(req.body);
+    let hashedPassword = bcrypt.hashSync(req.body.password, 10);
     const user = new User({
         firstName: req.body.firstName,
+        password:hashedPassword
         //etc...
     });
 
@@ -19,6 +23,28 @@ exports.register = (req, res) => {
             message:err.message || "Some error occured"
         })
     })
+}
+
+exports.login = (req, res) => {
+    //User.findOne (rechercher l'utilisateur par mail)
+    // then => trouve un utilisateur et toutes les propriétes
+    //comparer les deux mots de passe : req.body.password / data.password
+    //Si mot de pass non valide => envoye un message {auth:false, token:null}
+    //Sinon : 
+    let userToken = jwt.sign({
+        id: user._id,
+        isAdmin: user.isAdmin
+    },
+        configs.jwt.secret,
+        {
+            expiresIn: 86400
+        }
+    )
+
+    //envoyer le token {auth:true, token:userToken}
+
+    //Catch => pas d'utilisateurs
+
 }
 
 //updateUser
